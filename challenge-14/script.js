454cc05a1b14fdde51814fc4c0b6a094a933da1e
Harsha <harshabalaji77@gmail.com>
2026-03-05 11:28:17 +0530

    JS challenge-15 commit

--- challenge-14/script.js
+++ challenge-14/script.js
@@ -1,6 +1,6 @@
 /* Coding Challenge #4 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase. The input will come from a textarea inserted into the DOM (see code below to 
-insert the elements), and conversion will happen when the button is pressed.
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase. The input will come from a textarea inserted into the DOM 
+(see code below to insert the elements), and conversion will happen when the button is pressed.
 
 Test data (pasted to textarea, including spaces): 
 underscore_case 
